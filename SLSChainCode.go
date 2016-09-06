ee97fb8e758d80d364a288b24d60f058b97cbd54
Andriian Chestnykh <Andriian.Chestnykh@infopulse.local>
2016-09-06 19:14:22 +0300

    First working version of functions for Participants

--- SLSChainCode.go
+++ SLSChainCode.go
@@ -19,6 +19,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -26,20 +27,6 @@ import (
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
-
-type ParticipantType int
-
-//const (
-//	BANK ParticipantType = 1 + iota
-//	BORROWER
-//)
-
-type Participant struct {
-	participantName string
-	participantType string
-}
-
-var Participants []Participant
 
 // ============================================================================================================================
 // Main
@@ -52,12 +39,31 @@ func main() {
 }
 
 // Init resets all the things
-func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
+	//not to forget delete table is it already exists
+	stub.DeleteTable("Participants")
+
+	err := stub.CreateTable("Participants", []*shim.ColumnDefinition{
+		&shim.ColumnDefinition{Name: "Id", Type: shim.ColumnDefinition_UINT32, Key: true},
+		&shim.ColumnDefinition{Name: "Name", Type: shim.ColumnDefinition_STRING, Key: true},
+		&shim.ColumnDefinition{Name: "Type", Type: shim.ColumnDefinition_BYTES, Key: false},
+	})
+
+	if err != nil {
+		return nil, errors.New("Failed creating Participants table: " + err.Error())
+	}
+
+	err2 := stub.PutState("ParticipantsQuantity", []byte(strconv.Itoa(0)))
+	if err2 != nil {
+		return nil, errors.New("Failed to add participants quantity to state")
+	}
+
 	return nil, nil
 }
 
 // Invoke is our entry point to invoke a chaincode function
-func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
 	// Handle different functions
@@ -65,7 +71,7 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 		return t.Init(stub, "init", args)
 	}
 	if function == "addParticipant" { //initialize the chaincode state, used as reset
-		return t.Init(stub, "addParticipant", args)
+		return nil, t.addParticipant(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function) //error
 
@@ -73,15 +79,14 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 }
 
 // Query is our entry point for queries
-func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "getParticipantsList" { //read a variable
-		i, err := t.getParticipantsList(stub, args)
-		fmt.Println("Function " + function + " has been found") //error
+	if function == "getParticipantsQuantity" { //read a variable
+		q, err := t.getParticipantsQuantity(stub, args)
 		if err == nil {
-			fmt.Println("Quantity of participants: " + string(i))
+			fmt.Println("Quantity of participants: " + q)
 			return nil, nil
 		}
 	}
@@ -94,32 +99,56 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 //Two arguments expected:
 //Participant Name (string)
 //Participant Type (string) BANK, BORROWER
-func (t *SimpleChaincode) addParticipant(stub *shim.ChaincodeStub, args []string) error {
+func (t *SimpleChaincode) addParticipant(stub shim.ChaincodeStubInterface, args []string) error {
 	if len(args) != 2 {
 		return errors.New("Incorrect number of arguments. Expecting 2")
 	}
 
-	var p Participant
-	p.participantName = args[0]
-	p.participantType = args[1]
+	qBytes, _ := stub.GetState("ParticipantsQuantity")
+	if qBytes == nil {
+		return errors.New("Participants quantity entity not found")
+	}
+	q, _ := strconv.Atoi(string(qBytes))
+	q++
 
-	Participants = append(Participants, p)
+	//Add participant to ledger table
+	ok, err := stub.InsertRow("Participants", shim.Row{
+		Columns: []*shim.Column{
+			&shim.Column{Value: &shim.Column_Int32{Int32: int32(q)}},
+			&shim.Column{Value: &shim.Column_String_{String_: args[0]}},
+			&shim.Column{Value: &shim.Column_String_{String_: args[1]}}},
+	})
+	if !ok && err == nil {
+		return errors.New("Participant " + args[0] + " already assigned")
+	}
 
-	fmt.Println("Put to ledger Participant: " + args[0] + " Type: " + args[1])
+	//Update total participants quantity
+	err2 := stub.PutState("ParticipantsQuantity", []byte(strconv.Itoa(q)))
+
+	if err2 != nil {
+		return errors.New("Failed to add Participant")
+	}
+
+	fmt.Println("Participant: " + args[0] + " Type: " + args[1] + " has been put to ledger")
 	return nil
 }
 
-func (t *SimpleChaincode) getParticipantsList(stub *shim.ChaincodeStub, args []string) (int, error) {
-	//	for i := 0; i < len(participants); i++ {
-	//		s = s + "Participant Name: " + participants[i].participantName + " Participant Type: " + participants[i].participantType
-	//	}
+func (t *SimpleChaincode) getParticipantsQuantity(stub shim.ChaincodeStubInterface, args []string) (string, error) {
 
-	i := len(Participants)
-	return i, nil
+	qBytes, err := stub.GetState("ParticipantsQuantity")
+	if qBytes == nil {
+		return "", errors.New("Participants quantity entity not found")
+	}
+
+	if err != nil {
+		return "", errors.New("Failed to get participants quantity")
+	}
+
+	return string(qBytes), nil
 }
 
 //2. Arranger Bank: send Loan invitation to Borrower
-func (t *SimpleChaincode) sendLoanInvitation(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+func (t *SimpleChaincode) sendLoanInvitation(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	return nil, nil
 }
 
